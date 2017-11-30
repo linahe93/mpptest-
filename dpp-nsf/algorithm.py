7d15f045270211997e946bfb43790e1f176b33de
Lina He <linahe93@gmail.com>
2017-11-30 21:17:09 +0100

    Add files via upload

---
+++ dpp-nsf/algorithm.py
@@ -0,0 +1,23 @@
+from DisjointedNode import path_computing
+from SpectrumAssign import routing_and_spectrum
+import networkx as nx
+
+
+def algorithm(graph, flow):
+    src = flow.src_num
+    dst = flow.dst_num
+    required_bandwidth = flow.bandwidth
+
+    print "3: path computing"
+    shortest_paths = path_computing(graph, src, dst, k=3)
+    shortest_paths.sort(key=len)
+    # print "the result of disjoint paths for src %s and dst %s:" % (src, dst),
+    print shortest_paths
+
+    print "4: spectrum assigning"
+    paths_occupied_spectrum = routing_and_spectrum(graph, shortest_paths, required_bandwidth)
+    # print "all routing path and occupied slots %s" % paths_occupied_spectrum
+    # print "the graph after the spectrum assignment %s" % graph.edges(data=True)
+
+    return paths_occupied_spectrum
+
---
+++ dpp-nsf/flow.py
@@ -0,0 +1,7 @@
+class Flow(object):
+    def __init__(self, src_num, dst_num, start_time, finish_time, bandwidth):
+        self.src_num = src_num
+        self.dst_num = dst_num
+        self.start_time = start_time
+        self.finish_time = finish_time
+        self.bandwidth = bandwidth
